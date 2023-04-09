07d78687c144f47b0047e19ca87641f492714cb0
Matei <matei.ciocanescu10@gmail.com>
2023-04-09 18:54:31 +0300

    Add project files.

---
+++ Candidate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityEnrollment
+{
+    internal class Candidate
+    {
+        const int candidateId = 0;
+        StringBuilder lastName = new StringBuilder(20); //note: use ToString to get the actual string
+        StringBuilder firstName = new StringBuilder(20);
+
+    }
+}
---
+++ Faculty.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityEnrollment
+{
+    internal class Faculty
+    {
+        const int facultyId = 0;
+        StringBuilder facultyName = new StringBuilder();
+
+    }
+}
